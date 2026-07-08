8739e7667942c2e07898d3992c6a7faf6f74bac6
Gerry Fernando <firmanaldif3@gmail.com>
2026-07-08 22:46:53 +0700

    add get api json data

--- app/_components/Product/index.tsx
+++ app/_components/Product/index.tsx
@@ -1,12 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import ProductFilter from "./ProductFIlter";
 
+type Product = {
+  name: string;
+  image: string;
+  id: number;
+};
 export default function Product() {
-  const products = Array.from({ length: 6 }, (_, i) => ({
-    id: i,
-    name: `Speaker ${890 + i}`,
-    image: `${i + 1}.svg`,
-  }));
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const res = await fetch("/api/product");
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch products");
+      }
+
+      const data: Product[] = await res.json();
+      setProducts(data);
+    }
+
+    getProducts();
+  }, []);
 
   return (
     <main className="min-h-screen text-white">
